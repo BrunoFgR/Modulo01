ff755073530e18bce8bbdac200dc56e600e57d4c
BrunoFgR <brunofds281@gmail.com>
2020-05-02 18:44:39 -0300

    feat: Projeto finalizado

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,71 @@ app.use(cors());
 
 const repositories = [];
 
+function checkId(request, response, next) {
+  const { id } = request.params;
+
+  if(!isUuid(id)){
+    return response.status(400).json({ error: 'the id does not exist'})
+  }
+
+  return next();
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const data = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0  
+  }
+
+  repositories.push(data);
+
+  return response.json(data)
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", checkId, (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repIndex = repositories.findIndex(r => r.id === id);
+  const folder = repositories.find(r => r.id === id);
+
+  const data = {...folder, title, url, techs};
+
+  repositories[repIndex] = data;
+
+  return response.json(data);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", checkId, (request, response) => {
+  const { id } = request.params;
+
+  const repIndex = repositories.findIndex(r => r.id === id);
+
+  repositories.splice(repIndex, 1)
+
+  return response.status(204).send()
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", checkId, (request, response) => {
+  const { id } = request.params;
+
+  const repIndex = repositories.findIndex(r => r.id === id);
+  const rep = repositories.find(r => r.id === id);
+
+  const data = { ...rep, likes: rep.likes += 1 };
+
+  repositories[repIndex] = data;
+
+  return response.json(data);
 });
 
 module.exports = app;
